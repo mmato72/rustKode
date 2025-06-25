5f640354bcc1798bb29c990963d765f03ed51b22
Martin Mato <martin.mato@ymail.com>
2025-06-25 18:23:32 +0100

    A new day dawns

---
+++ hello_cargo/target/debug/deps/hello_cargo-4bb435b1b3a47e80.d
@@ -0,0 +1,5 @@
+/home/martin/repos/rustKode/hello_cargo/target/debug/deps/libhello_cargo-4bb435b1b3a47e80.rmeta: src/main.rs
+
+/home/martin/repos/rustKode/hello_cargo/target/debug/deps/hello_cargo-4bb435b1b3a47e80.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-4f8f4b4faaf615cd.d
@@ -0,0 +1,5 @@
+/home/martin/repos/rustKode/hello_cargo/target/debug/deps/libhello_cargo-4f8f4b4faaf615cd.rmeta: src/main.rs
+
+/home/martin/repos/rustKode/hello_cargo/target/debug/deps/hello_cargo-4f8f4b4faaf615cd.d: src/main.rs
+
+src/main.rs:
